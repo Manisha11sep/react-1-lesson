4bec1284f718aa9dd8d04a50af282c373026777e
Manisha11sep <er.manisha11sep@gmail.com>
2018-03-16 14:32:33 -0700

    step 5

--- src/App.js
+++ src/App.js
@@ -12,16 +12,45 @@ class App extends Component {
 
     };
   }
+
+changePicture(value)
+{
+  this.setState( () =>
+  {
+    return{
+      pictureUrl:value,
+    }
+  })
+}
+changeName(value){
+  this.setState (() =>
+  {
+    return{
+ name:value,
+  }
+})
+}
+
+
+
+
   render() {
     return (
       <div >
         Picture:
-        <input/>
+      <input onChange = {event =>this.changePicture(event.target.value)}
+      />
+        {/* <input onChange = {event =>
+  {
+    const newValue = event.target.value;
+    this.changeState(newValue);
+  }}
+    */}
         Name:
-        <input />
+        <input onChange = {event =>this.changeName(event.target.value)}/>
         <button>
           Add Friend </button>
-        
+        <div> Current State: {JSON.stringify(this.state)}</div> 
       </div>
     );
   }
